146a114a52b0bc62d43f8e134a498f5462654113
GDReadwin <gdread3@gmail.com>
2025-01-22 20:59:22 -0700

    Update words.ts

--- src/data/words.ts
+++ src/data/words.ts
@@ -1,4 +1,6 @@
-export const WORDS = [
+//originally the test words for the app. this will later be updated to be a backup database of words to enable offline play or as a failsafe is the apis are not able to pull words
+
+export const BACKUPWORDS = [
     'REACT',
     'TYPESCRIPT',
     'JAVASCRIPT',
