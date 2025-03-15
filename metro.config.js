cb8efa08ac0dde4634ad20130f4e3b0c9cda9881
GDRead3 <gdread3@gmail.com>
2025-03-15 09:25:05 -0600

    updated packages
    updated packages for new metro and android. still does not work with QR scanning but it does work with 'upsidedowncake' emulator phones

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+// This is the Metro configuration file for React Native
+// https://facebook.github.io/metro/docs/configuration
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ */
+const config = getDefaultConfig(__dirname);
+
+// Add any custom configuration here if needed
+// For example, if you have specific file extensions to handle
+
+module.exports = config;
